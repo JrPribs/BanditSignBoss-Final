4353cf0e0fd3cc8c86f47d2480249873311dfbb6
theApparatus <paniceye@gmail.com>
2014-11-13 02:25:26 -0500

    updating upload page
    support for single file, disabled input button if no images are
    selected, added styling, fixed crash when images don't contain gps data,
    added lodash lib

--- public/javascripts/uploadPreview.js
+++ public/javascripts/uploadPreview.js
@@ -1,35 +1,48 @@
+var selDiv = "";
 
-    var selDiv = "";
-        
-    document.addEventListener("DOMContentLoaded", init, false);
-    
-    function init() {
-        document.querySelector('#imageFiles').addEventListener('change', handleFileSelect, false);
-        selDiv = document.querySelector("#selectedFiles");
-    }
-        
-    function handleFileSelect(e) {
-        
-        if(!e.target.files || !window.FileReader) return;
-        
-        selDiv.innerHTML = "";
-        
-        var files = e.target.files;
-        var filesArr = Array.prototype.slice.call(files);
+document.addEventListener("DOMContentLoaded", init, false);
 
-        filesArr.forEach(function(f) {
-            if(!f.type.match("image.*")) {
-                return;
-            }
-    
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                var html = '<img src="' + e.target.result + '" width="50px" height="50px">' + f.name + '<label for="' + f.name + '">Photo Comment</label><input type="text" name="' + f.name + '"><br/>';
-                selDiv.innerHTML += html;               
-            }
-            reader.readAsDataURL(f);
-        });
-        
-        
+function thumbnailContainer(thumbnail, caption) {
+    caption = caption || ''
+    return ['<div class="col-xs-4 col-md-3">',
+        '<span class="thumbnail">',
+        thumbnail,
+        '</span>',
+        '<label>Filename </label> ' + caption,
+        '<br/><label for="' + caption + '">Photo Comment </label> <input type="text" name="' + caption + '">',
+        '</div>'
+    ].join('\n');
+
+};
+
+
+function init() {
+    document.querySelector('#imageFiles').addEventListener('change', handleFileSelect, false);
+    selDiv = document.querySelector("#selectedFiles");
+}
+
+function handleFileSelect(e) {
+    if (e.target.files.length !== 0) {
+        document.querySelector('#upload').disabled = false;
     }
-    
+    if (!e.target.files || !window.FileReader) return;
+    selDiv.innerHTML = "";
+
+    var files = e.target.files;
+    var filesArr = Array.prototype.slice.call(files);
+
+    filesArr.forEach(function(f) {
+        if (!f.type.match("image.*")) {
+            return;
+        }
+
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            var html = '<img src="' + e.target.result + '">';
+            selDiv.innerHTML += thumbnailContainer(html, f.name);
+        }
+        reader.readAsDataURL(f);
+    });
+
+
+}
